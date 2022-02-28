a788c8bdc5a449e8c3dea6a2b81b7b2523c09731
sadiqli1 <isaas@code.edu.az>
2022-02-28 23:29:19 +0400

    created folder

---
+++ HomeWork-02.29.2022/Extension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_02._29._2022
+{
+    static class Extension
+    {
+        #region HomeWork1
+        //public static string toCaptalized(this string word)
+        //{
+        //    word = word.ToLower();
+        //    string[] newword = word.Split(' ');
+        //    StringBuilder sb = new StringBuilder();
+        //    foreach (string item in newword)
+        //    {
+        //        sb.Append(char.ToUpper(item[0]));
+        //        for (int i = 1; i < item.Length; i++)
+        //        {
+        //            sb.Append(item[i]);
+        //        }
+        //        sb.Append(" ");
+
+        //    }
+        //    return sb.ToString();
+        #endregion
+
+        #region HomeWork2
+        //public static bool Search(this string txt, string search)
+        //{
+        //    string[] newtxt = txt.Split(" ");
+        //    foreach (string item in newtxt)
+        //    {
+        //        if (item == search)
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+        #endregion
+    }
+
+
+}
---
+++ HomeWork-02.29.2022/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork_02._29._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region HomeWork1
+            //string name = "exTensiON claSSi staTIc OLMalIdiR";
+            //Console.WriteLine(name.toCaptalized());
+            #endregion
+
+            #region HomeWork2
+            //string _txt = "Salam dunya necesen";
+            //Console.WriteLine(_txt.Search("salam"));
+            #endregion
+        }
+    }
+}
